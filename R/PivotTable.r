b9362fbeeb9a666d84c9a2c61d5e0310b0191d94
RemiHuangNTHU <yulieh.huang@gmail.com>
2019-01-05 15:04:41 +0800

    no message

--- R/PivotTable.r
+++
@@ -1,8 +0,0 @@
-library(rpivotTable)                            # rpivotTable 套件
-
-### 基本設定 ###
-r.File = "C:/Remi/Remi Moocs/Data/Cars.csv"     # 設定檔案路徑
-CSV    = read.csv(r.File)                       # 讀取 csv 檔
-
-### 利用 rpivotTable 套件 ###
-rpivotTable(CSV)                                # 進行 pivotal analysis
